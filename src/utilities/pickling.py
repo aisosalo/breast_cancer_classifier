2884cb6bb07be01d700ce026bbdde392df5b934e
Antti Isosalo <34138494+aisosalo@users.noreply.github.com>
2020-05-10 01:10:07 +0300

    Update pickling.py

--- src/utilities/pickling.py
+++ src/utilities/pickling.py
@@ -31,6 +31,7 @@ def pickle_to_file(file_name, data, protocol = pickle.HIGHEST_PROTOCOL):
     with open(file_name, 'wb') as handle:
         pickle.dump(data, handle, protocol)
 
+        
 def unpickle_from_file(file_name):
     with open(file_name, 'rb') as handle:
         return pickle.load(handle)
